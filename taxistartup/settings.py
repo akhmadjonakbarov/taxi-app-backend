0b54540f45d1992d0dc8f9544e8849e4266fa5d8
akhamdjonakbarov <akhmadjonakbarov@gmail.com>
2023-01-26 01:12:39 +0500

    updated all files

--- taxistartup/settings.py
+++ taxistartup/settings.py
@@ -175,15 +175,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # MEDIA_URL = '/images/'
-# MEDIA_URL = ''
-#
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'statics'
-# ]
-#
-# MEDIA_ROOT = joinpath(BASE_DIR, 'statics/images')
-#
-# STATIC_ROOT = joinpath(BASE_DIR, 'staticfiles')
+MEDIA_URL = ''
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics'
+]
+
+MEDIA_ROOT = joinpath(BASE_DIR, 'statics/images')
+
+STATIC_ROOT = joinpath(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
