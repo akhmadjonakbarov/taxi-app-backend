fa612c380e847ce7a159e5a85522983217866893
akhamdjonakbarov <akhmadjonakbarov@gmail.com>
2023-01-30 01:30:37 +0500

    url of login and register view was corrected

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.RoutesView.as_view()),
     path('auth/user/login/', views.LoginView.as_view(), name='login'),
     path('auth/user/register/', views.RegisterView.as_view(), name='register'),
+    path('auth/user/', views.GetUserView.as_view()),
     path('services/', views.ServiceGetView.as_view()),
     path('cudservices/', views.ServiceCUDView.as_view())
 ]
